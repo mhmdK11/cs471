10eb784b78ac31cc207170f58d3e08df7e244e4b
mhmd <mkma053@gmail.com>
2025-02-28 03:28:15 +0300

    lab4

--- CS471/libraryproject/apps/bookmodule/views.py
+++ CS471/libraryproject/apps/bookmodule/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def index(request):
+"""def index(request):
     name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html"  , {"name": name}) 
+    return render(request, "bookmodule/index.html"  , {"name": name}) """
 
 def index2(request, val1 = 0): 
  return HttpResponse("value1 = "+str(val1))
@@ -18,5 +18,12 @@ def viewbook(request, bookId):
  context = {'book':targetBook} 
  return render(request, 'bookmodule/show.html', context)
 
-
+def index(request):
+ return render(request, "bookmodule/index.html")
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
  
