24f2c51d9feae0f25855e69615d59e681f7b9b1d
mhmd <mkma053@gmail.com>
2025-03-06 04:58:57 +0300

    lab5

--- CS471/libraryproject/apps/bookmodule/urls.py
+++ CS471/libraryproject/apps/bookmodule/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
  path('list_books/', views.list_books, name= "books.list_books"),
  path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
  path('aboutus/', views.aboutus, name="books.aboutus"),
-
+ path('html5/links/', views.links, name = "books.html5.links"),
+ path('html5/text/formatting', views.formatting, name = "books.html5.text.formating"),
+ path('html5/listing', views.listing, name = "books.html5.listing"),
+ path('html5/tables', views.tables, name = "books.html5.tables"),
 ]
